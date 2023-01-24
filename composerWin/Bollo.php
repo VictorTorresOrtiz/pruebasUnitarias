d47f00c35eb71855ad2f0e974be5871906d01b69
Victor Torres Ortiz <victor.torres.ortiz.18@gmail.com>
2023-01-24 11:38:56 +0100

    OK errores

--- composerWin/Bollo.php
+++ composerWin/Bollo.php
@@ -1,14 +1,32 @@
 <?php 
-    include_once "Dulces.php";
-    class Bollo extends Dulces{
-        private $relleno;
-        public function __construct($nombre,$numero,$precio,$relleno){
-            parent::__construct($nombre,$numero,$precio);
-            $this->relleno=$relleno;
-        }
-        public function muestraResumen(){
-            parent:: muestraResumen();
-            echo "<br>Relleno: $this->relleno";
-        }
+include_once("vendor/autoload.php");
+
+class Bollo extends Dulces
+{
+
+    public function __construct(
+        string $nombre,
+        int $numero,
+        float $precio,
+        private string $relleno
+    ) {
+        parent::__construct($nombre, $numero, $precio);
     }
+
+    public function getRelleno(): string
+    {
+        return $this->relleno;
+    }
+
+
+    public function setRelleno($relleno): void
+    {
+        $this->relleno = $relleno;
+    }
+
+    public function muestraResumen(): void
+    {
+        echo parent::muestraResumen() . "<p>El relleno: " . $this->getRelleno() . "</p>";
+    }
+}
 ?>
--- composerWin/Chocolate.php
+++ composerWin/Chocolate.php
@@ -1,17 +1,43 @@
 <?php 
-    include_once "Dulces.php";
-    class Chocolate extends Dulces{
-        private $porcentajeCacao;
-        private $peso;
-        public function __construct($nombre,$numero,$precio,$porcentajeCacao,$peso){
-            parent::__construct($nombre,$numero,$precio);
-            $this->porcentajeCacao=$porcentajeCacao;
-            $this->peso=$peso;
-        }
-        public function muestraResumen(){
-            parent:: muestraResumen();
-            echo "<br>Porcentaje de Cacao: $this->porcentajeCacao %";
-            echo "<br>Peso: $this->peso g";
-        }
+include_once("vendor/autoload.php");
+
+class Chocolate extends Dulces
+{
+
+    public function __construct(
+        string $nombre,
+        int $numero,
+        float $precio,
+        private float $porcentajeCacao,
+        private float $peso
+    ) {
+        parent::__construct($nombre, $numero, $precio);
     }
+
+    public function getPorcentajeCacao(): float
+    {
+        return $this->porcentajeCacao;
+    }
+
+    public function setPorcentajeCacao($porcentajeCacao): void
+    {
+        $this->porcentajeCacao = $porcentajeCacao;
+    }
+
+    public function getPeso(): float
+    {
+        return $this->peso;
+    }
+
+    public function setPeso($peso): void
+    {
+        $this->peso = $peso;
+    }
+
+    public function muestraResumen(): void
+    {
+        echo parent::muestraResumen() . "<p>%Cacao: " . $this->getPorcentajeCacao() . "</p>
+        <p>Peso: " . $this->getPeso() . "g</p>";
+    }
+}
 ?>
